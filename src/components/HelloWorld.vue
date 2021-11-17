dfd8f114dd929aacbcb16aeb6cd9abc4cac301c2
YohanesF <yohannesfransisco@gmail.com>
2021-11-17 19:06:44 +0700

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1> Yohanes Fransisco(2024250088) </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
